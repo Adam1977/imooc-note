1d5a32638150eb60d89bfb24b511c94c466e3ac2
adam_sgl <1024844072@qq.com>
2020-04-07 22:37:36 +0800

    add ebook-demo

---
+++ ebook-demo/ebook-demo/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <span class="icon-back"></span>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created () {
+    // document.addEventListener('DOMContentLoaded', () => {
+    //   console.log(123123)
+    //   const html = document.querySelector('html')
+    //   html.style.fontSize = window.innerWidth / 10 + 'px'
+    // })
+  }
+}
+document.addEventListener('DOMContentLoaded', () => {
+  console.log(123123)
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 20 ? 20 : fontSize
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ ebook-demo/ebook-demo/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import '@/assets/css/global.scss'
+import 'normalize.css'
+import '@/assets/icomoon/icon.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
